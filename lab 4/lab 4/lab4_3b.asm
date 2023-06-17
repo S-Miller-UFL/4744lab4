de0287174b6677ddc70732dc3bb703cd61dd9de9
steven miller <sjmiller0403@gmail.com>
2023-06-17 15:10:12 -0400

    almost finished, just need to clean up the prelab and finish the prelab questions.

--- lab 4/lab 4/lab4_3b.asm
+++ lab 4/lab 4/lab4_3b.asm
@@ -1,18 +1,23 @@
 ;*******************************************
+;Lab 4, Section 3b
 ;Name: Steven Miller
 ;Class #: 11318
 ;PI Name: Anthony Stross
-;Description: retrieves values from the EBI data bus and outputs them to the backpack leds
+;Description: Writes switch values from the external switches to the external LED's
 ;*******************************************
 ;***************INCLUDES*************************************
 .include "ATxmega128a1udef.inc"
 ;***************END OF INCLUDES******************************
 
 ;*********************************EQUATES********************************
+.EQU STACKEND = 0x3fff ;top of stack
+.EQU STACKBEGIN = 0x2000 ;bottom of stack
 .EQU INPUT = 0B00000000
 .EQU OUTPUT = 0B11111111
 .EQU IO_START_ADDRESS = 0X224000
 .EQU SRAM_START_ADDRESS = 0X128000
+.EQU READ_LOCATION = 0X128000
+.EQU WRITE_LOCATION = 0X128100
 ;*******************************END OF EQUATES*******************************
 
 ;*********************************DEFS********************************
@@ -27,32 +32,34 @@
 	RJMP MAIN
 .ORG 0X0200
 MAIN:
+	;INITIALIZE STACK POINTER
+	LDI R16, LOW(STACKBEGIN)
+	out CPU_SPL, R16
+	LDI R16, HIGH(STACKEND)
+	OUT CPU_SPH, R16
 	RCALL EBI_INIT
-	;INIT BACKPACK IO (TOO LAZY FOR SUBROUTINE)
-	LDI R16, 0XFF
-	STS PORTC_DIRSET, R16
-	LDI R16, 0X00
-	STS PORTC_OUT, R16
-	;LOAD IO ADDRESS
-	LDI XL, BYTE1(IO_START_ADDRESS)
-	LDI XH, BYTE2(IO_START_ADDRESS)
-	LDI R16, BYTE3(IO_START_ADDRESS)
-	sts CPU_RAMPX, R16
-	;LOAD SRAM ADDRESS
+	;LOAD SRAM ADDRESS INTO Y
 	LDI YL, BYTE1(SRAM_START_ADDRESS)
 	LDI YH, BYTE2(SRAM_START_ADDRESS)
 	LDI R16, BYTE3(SRAM_START_ADDRESS)
 	OUT CPU_RAMPY, R16
-	ldi r16, byte1(io_start_address)
-	STS PORTK_OUT, R16
-	;SET RE AND WE TO TRUE
-	LDI R16, 0B00000011
-	STS PORTH_OUTCLR, R16
+	;LOAD WRITE ADDRESS INTO Z
+	LDI ZL, BYTE1(WRITE_LOCATION )
+	LDI ZH, BYTE2(WRITE_LOCATION )
+	LDI R16, BYTE3(WRITE_LOCATION )
+	OUT CPU_RAMPZ, R16
+	;INITIALIZE SRAM ADDRESSES
+	LDI R16, 0XE
+	ST Y,R16
+	LDI R16, 0XF
+	ST Z,R16
 	LOOP:
-	;GET DATA FROM DATA BUS
-	ld r16, x
-	STS PORTC_OUT, R16
-	st x, r16
+	;READ FROM SRAM READ ADDRESS
+	LD R16,Y
+	;LOAD R16 WITH 0XF
+	LDI R16, 0XF
+	;WRITE TO SRAM WRITE ADDRESS
+	ST Z, r16
 	RJMP LOOP
 END:
 RJMP END
@@ -79,8 +86,6 @@ EBI_INIT:
 	;SET ADDRESS SIGNALS TO OUTPUT
 	LDI R16,0XFF
 	STS PORTK_DIRSET, R16
-	;SET DATA BUS TO INPUT
-	;STS PORTJ_DIRCLR, R16
 	;SET EBI TYPE TO 3 PORT SRAM ALE1
 	LDI R16, 0B00000001
 	STS EBI_CTRL, R16
